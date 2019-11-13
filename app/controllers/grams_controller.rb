cdb285869a5b5ff3a51632de822f0e0ee10054da
donghanbing1989@gmail.com <donghanbing1989@gmail.com>
2019-11-13 00:21:00 +0000

    Updating Grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -10,16 +10,31 @@ class GramsController < ApplicationController
 
 	def show
 		@gram =Gram.find_by_id(params[:id])
-		if @gram.blank?
-    	render plain: 'Not Found :(', status: :not_found
-  	end
+		return render_not_found if @gram.blank?
+		# if @gram.blank?
+  #   	render plain: 'Not Found :(', status: :not_found
+  # 	end
 	end
 
 	def edit
 		@gram = Gram.find_by_id(params[:id])
-		if @gram.blank?
-			render plain: 'Not Found :(', status: :not_found
-		end
+		return render_not_found if @gram.blank?
+		# if @gram.blank?
+		# 	render plain: 'Not Found :(', status: :not_found
+		# end
+	end
+
+	def update
+  	@gram = Gram.find_by_id(params[:id])
+  	return render_not_found if @gram.blank?
+
+  	@gram.update_attributes(gram_params)
+
+  	if @gram.valid?
+    	redirect_to root_path
+  	else
+    	return render :edit, status: :unprocessable_entity
+  	end
 	end
 
 	def create
@@ -35,5 +50,9 @@ class GramsController < ApplicationController
 
 	def gram_params
 		params.require(:gram).permit(:message)
+	end
+
+	def render_not_found
+		render plain: 'Not Found :(', status: :not_found
 	end
 end
